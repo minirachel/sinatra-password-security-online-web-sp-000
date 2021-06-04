997835e09aa07a9f59d6f5d3fbdd24b95de1fdfe
minirachel <rachel.a.levinson@gmail.com>
2021-06-04 13:53:06 -0400

    commit tree 5e0c6c2ac49e487e5f37151441799747edb11fd5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
+    #active record macro, works with bcrypt and gives us abilities to create methods without storing plaintext in database
 end
